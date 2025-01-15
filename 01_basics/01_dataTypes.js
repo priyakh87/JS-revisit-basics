b97b298549e741a45b530177ba53b01dc7f00f25
Priya Khanna <144544392+priyakh87@users.noreply.github.com>
2025-01-15 11:39:16 +0000

    added variables and datatypes basic

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all JS code as newer version
+
+
+let nbr=888;
+console.log(nbr);
+let binaryNbr=0B11111;
+
+console.log(binaryNbr);
+let userName="priya";
+console.log(userName);
+
+let userDob;
+console.log(userDob);
+
+let userAddress=null;
+console.log(userAddress);
+
+console.log(typeof nbr, typeof userAddress, typeof userDob);
+
+
+
+
+
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=20;
+let accountEmail="pk@gmail.com";
+var accountUserName='Priya khanna';
+accountCity="Dubai";
+
+// accountId=2;
+
+console.log(accountId);
+accountEmail="abc@gmail.com";
+accountUserName="pppppp";
+accountCity="delhi";
+
+
+console.table([accountEmail,accountUserName,accountCity]);
