3bd7aea002ea9274494045be785508fada912e06
Priya Khanna <144544392+priyakh87@users.noreply.github.com>
2025-01-15 11:58:21 +0000

    type conversions

--- 01_basics/01_dataTypes.js
+++ 01_basics/01_dataTypes.js
@@ -1,7 +1,7 @@
 "use strict";//treat all JS code as newer version
 
 
-let nbr=888;
+let nbr=2;//if this value is converted to Number then its value is not a number (NaN)
 console.log(nbr);
 let binaryNbr=0B11111;
 
@@ -17,7 +17,19 @@ console.log(userAddress);
 
 console.log(typeof nbr, typeof userAddress, typeof userDob);
 
+// type conversions
+
+let valueInNumber=Number(nbr);
+console.log(valueInNumber, typeof valueInNumber);
+
+let valueInString=String(nbr);
+console.log(valueInString , typeof valueInString);
+
+let valueInSymbol=Symbol(nbr);
+console.log(valueInSymbol , typeof valueInSymbol);
 
 
+let valueInObject=Object(nbr);
+console.log(valueInObject , typeof valueInObject);
 
 
